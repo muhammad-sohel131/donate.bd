a17255a38f94448fb1c61471c1fb69eb51ccc16a
= <sohelf131@gmail.com>
2024-09-23 21:47:42 +0600

    made responsive nav for mobile and added the functionality to show and hide the cards and history

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hi")
+const donationBtn = document.getElementById("donationBtn");
+const historyBtn = document.getElementById("historyBtn");
+
+donationBtn.addEventListener("click", showSection);
+historyBtn.addEventListener("click", showSection);
+
+function showSection(event){
+    const target = event.target.id;
+    
+    document.getElementById(target).classList.remove('white-btn');
+
+    let targetId;
+    let currentSection;
+    let targetSection;
+    if(target == "donationBtn"){
+        targetId = "historyBtn";
+        currentSection = "historySection";
+        targetSection = "cardSection";
+    }else {
+        targetId = "donationBtn";
+        currentSection = "cardSection";
+        targetSection = "historySection";
+    }
+
+
+    document.getElementById(targetId).classList.add("white-btn");
+    document.getElementById(currentSection).style.display = "none";
+    document.getElementById(targetSection).style.display = "block";
+
+    
+}
